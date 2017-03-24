c6273cd8adbf48d6ad625952dd8130d8c2e8d0ec
bene2929 <benedikt.kantz@htl-klu.at>
2017-03-24 17:26:48 +0100

    Deleted unneeded SmartHome Site

--- smart/php/DataHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST ['fileN'];
-echo ("$name\n");
-$myfile = fopen ( "$name", "w" ) or die ( "Unable to open file!" );
-$txt = $_POST ['msg'];
-fwrite ( $myfile, $txt );
-fclose ( $myfile );
-echo ("written" + "msg");
-?>
--- smart/web/DataHandler.php
+++
@@ -1,7 +0,0 @@
-<?php
-$myfile = fopen("data.txt", "w") or die("Unable to open file!");
-$txt = $_POST["suggest"];
-fwrite($myfile, $txt);
-fclose($myfile);
-echo("success");
-?>
